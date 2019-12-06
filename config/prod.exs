84d092e03fde36b447a6c706b4d450b2284808a2
teerawat1992 <teerawat.lamanchart@gmail.com>
2019-12-06 14:15:06 +0700

    Configure for production deployment

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :lotto_api, LottoAPIWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "serene-wildwood-76900", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
